7a42fde07c7125c2943d01a796bdec1b054c4539
Aditya-V-Rathi <rathiaditya.24@gmail.com>
2026-07-02 22:01:22 +0530

    Refactor sitemap function to simplify URL structure

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,10 +1,10 @@
+// app/sitemap.ts
 export default function sitemap() {
-  const baseUrl = "https://adityarathi.vercel.app";
   return [
-    { url: baseUrl, lastModified: new Date(), priority: 1 },
-    { url: `${baseUrl}/#about`, lastModified: new Date() },
-    { url: `${baseUrl}/#projects`, lastModified: new Date() },
-    { url: `${baseUrl}/#experience`, lastModified: new Date() },
-    { url: `${baseUrl}/#contact`, lastModified: new Date() },
+    {
+      url: "https://adityarathi.vercel.app",
+      lastModified: new Date(),
+      priority: 1,
+    },
   ];
 }
